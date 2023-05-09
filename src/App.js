7d4c3272486d4c90c5467181d7f17a671b3c6184
Bohdan Barulin <webshogun@proton.me>
2023-05-09 10:32:34 +0300

    Connected the OpenWeatherMap API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
+import { useState, useEffect } from 'react';
+import { WeatherIcons } from './WeatherIcons';
+
 const Home = () => {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${process.env.REACT_APP_API_KEY}`);
+        const weatherData = await response.json();
+        const { name, sys: {country}, main: {temp, pressure, humidity}, weather: [{icon, description}], wind: {speed} } = weatherData;
+        setWeather({ name, country, temp, icon, description, speed, humidity, pressure})
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchWeatherData();
+  }, [])
+
   return ( 
     <>
       <form className='search'>
         <input className='search-bar' type='text' placeholder='Type some city...' />
         <input className='search-button' type='submit' value='Search' />
       </form>
-      <h1 className='title'>Weather in New York, US</h1>
+      <h1 className='title'>Weather in {weather.name}, {weather.country}</h1>
       <div className='wrapper'>
-        <img className='icon' src='/icons/status/sunny.svg' alt='Weather status icon' />
+        <img className='icon' src={WeatherIcons[weather.icon]} alt='Weather status icon' />
         <div className='info'>
-          <span className='temp'>19.82°</span>
-          <p className='description'>Clear Sky</p>
+          <span className='temp'>{weather.temp}°</span>
+          <p className='description'>{weather.description}</p>
         </div>
       </div>
       <div className='wrapper'>
         <div className='wind'>
           <img src='/icons/wind.svg' alt='Wind icon' />
-          <span>1.6 m/s</span>
+          <span>{weather.speed} m/s</span>
         </div>
         <div className='humidity'>
           <img src='/icons/humidity.svg' alt='Humidity icon' />
-          <span>65%</span>
+          <span>{weather.humidity}%</span>
         </div>
         <div className='pressure'>
           <img src='/icons/pressure.svg' alt='Pressure icon' />
-          <span>1016</span>
+          <span>{weather.pressure}</span>
         </div>
       </div>
     </>
---
+++ src/WeatherIcons.js
@@ -0,0 +1,16 @@
+export const WeatherIcons = {
+  "01d": "/icons/status/sunny.svg",
+  "01n": "/icons/status/night.svg",
+  "02d": "/icons/status/day.svg",
+  "02n": "/icons/status/cloudy-night.svg",
+  "03d": "/icons/status/cloudy.svg",
+  "03n": "/icons/status/cloudy.svg",
+  "04d": "/icons/status/perfect-day.svg",
+  "04n": "/icons/status/cloudy-night.svg",
+  "09d": "/icons/status/rain.svg",
+  "09n": "/icons/status/rain-night.svg",
+  "10d": "/icons/status/rain.svg",
+  "10n": "/icons/status/rain-night.svg",
+  "11d": "/icons/status/storm.svg",
+  "11n": "/icons/status/storm.svg",
+};
